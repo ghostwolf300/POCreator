8f9554c446fe04a9ff24de2934a4a0ef356b0ef0
ville.susi <ville.susi@WAUTVANWS476.wihuri.local>
2021-02-18 21:13:51 +0200

    Does this work?

--- RFCService/src/main/java/org/rfc/material/template/TemplateRepository.java
+++
@@ -1,9 +0,0 @@
-package org.rfc.material.template;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TemplateRepository extends JpaRepository<Template,Integer>,TemplateRepositoryCustom {
-	
-	//public Template findById(int id);
-	
-}
